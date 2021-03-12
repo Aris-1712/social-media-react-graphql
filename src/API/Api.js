bc48ac07f586d4deda4e12471eab34fab9603b09
Aris <aris.gani@gmail.com>
2021-03-12 16:25:42 +0400

    Changes

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,2 +1,2 @@
-// export const Api='https://stark-ocean-73455.herokuapp.com/'
-export const Api='http://localhost:4000/'
+export const Api='https://stark-ocean-73455.herokuapp.com/'
+// export const Api='http://localhost:4000/'
